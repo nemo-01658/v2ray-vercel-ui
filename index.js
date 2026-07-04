f8609112eb213b68a355d36fc6cb14750eb1397c
ПΣMӨ <oshadairesh10@gmail.com>
2026-07-04 14:07:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('<h1>V2Ray Config Generator</h1><p>Coming soon...</p>');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Server running on port ' + port);
-});
+app.listen(process.env.PORT || 3000);
